be2604747b44df9006eeb3b6821a224b30e3a8d3
benatespina <benatespina@gmail.com>
2017-03-17 08:23:24 +0100

    Fixed header licenses

--- src/Priority/Priority.js
+++ src/Priority/Priority.js
@@ -1,12 +1,12 @@
 /*
  * This file is part of the EventBusJS library.
  *
- * Copyright (c) 2016 LIN3S <info@lin3s.com>
+ * Copyright (c) 2016-present LIN3S <info@lin3s.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @author Mikel Tuesta <mikel@lin3s.com>
+ * @author Mikel Tuesta <mikeltuesta@gmail.com>
  */
 
 'use strict';
