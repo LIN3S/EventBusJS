406cae0faf446ca1d066a134ae1a1a97c29b7eaa
Mikel Tuesta <mikel@lin3s.com>
2017-05-05 09:13:27 +0200

    Added AddDOMNode implementation. Pending related tests implementation & documentation.

---
+++ src/AddDOMNode/NodeAddedEventSubscriber.js
@@ -0,0 +1,35 @@
+/*
+ * This file is part of the EventBusJS library.
+ *
+ * Copyright (c) 2016-present LIN3S <info@lin3s.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Beñat Espiña <benatespina@gmail.com>
+ * @author Mikel Tuesta <mikeltuesta@gmail.com>
+ */
+
+'use strict';
+
+import EventSubscriber from './../EventSubscriber';
+import NodeAddedEvent from './NodeAddedEvent';
+
+class NodeAddedEventSubscriber extends EventSubscriber {
+
+  domSelectorClassName;
+
+  constructor(aCallback, aPriority, domSelectorClassName) {
+    super(aCallback, aPriority);
+
+    this.domSelectorClassName = domSelectorClassName;
+  }
+
+  isSubscribedTo(anEvent) {
+    const event = new NodeAddedEvent();
+
+    return anEvent.getName() === event.getName() && this.domSelectorClassName === anEvent.domSelectorClassName;
+  }
+}
+
+export default NodeAddedEventSubscriber;
